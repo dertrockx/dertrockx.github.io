5e2af247435329f9e760bbccba49f3a23159a568
Ian Salazar <iisalazar@up.edu.ph>
2019-12-24 14:04:17 +0800

    Updates

---
+++ precache-manifest.22130f49b668a47aa8408a10413be6fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0419dd7f4db282f4d4e676d52c958a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17a2d8e3382a9634302d",
+    "url": "/static/css/main.5634d8e0.chunk.css"
+  },
+  {
+    "revision": "b501ebfa18fd992a4245",
+    "url": "/static/js/2.95e79b48.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/static/js/2.95e79b48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17a2d8e3382a9634302d",
+    "url": "/static/js/main.31c17378.chunk.js"
+  },
+  {
+    "revision": "00ccf1beb4d435cfbe60",
+    "url": "/static/js/runtime-main.c7cc6e23.js"
+  },
+  {
+    "revision": "a9cd0e2398d5ac3dab954fb815d27227",
+    "url": "/static/media/code.a9cd0e23.jpg"
+  },
+  {
+    "revision": "00bdcbb9bcdea9bf1556d9661a6a1f5e",
+    "url": "/static/media/me.00bdcbb9.jpg"
+  }
+]);
